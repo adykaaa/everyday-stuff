cb239be387e92b294fdc53dfd843ac264d2e391d
adykaaa <gosupalanta@gmail.com>
2022-08-17 21:59:38 +0200

    Go 1.19

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-#installing latest go version
+#installing Go 1.19
 wget https://go.dev/dl/go1.19.linux-amd64.tar.gz && \
   sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz && \
   echo "export PATH=$PATH:/usr/local/go/bin \n" >> ~/.zshrc && \
