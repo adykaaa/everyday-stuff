48b730c1d7c07ed31577b6687492b265d890c57d
adykaaa <gosupalanta@gmail.com>
2022-08-20 18:24:02 +0200

    adding k3d install to setup.sh

--- setup.sh
+++ setup.sh
@@ -56,4 +56,7 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 sudo apt update && sudo apt-get install terraform -y
 python3 -m pip install --user ansible
 
+#installing K3d
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d cluster create local-cluster
 echo " \n \n \n ==== Copy .vimrc from the repo, then open vim and do PlugInstall, CocInstall coc-pyright, CocInstall coc-tsserver, CocInstall coc-go ===="
