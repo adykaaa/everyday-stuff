c2e5950e0a9b90d64853ea34d29c9d80499809b4
adykaaa <gosupalanta@gmail.com>
2022-10-28 12:11:45 +0200

    Update nvim-tree.lua

--- .config/lua/josean/plugins/nvim-tree.lua
+++ .config/lua/josean/plugins/nvim-tree.lua
@@ -4,6 +4,14 @@ if not setup then
 	return
 end
 
+local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+if not config_status_ok then
+	return
+end
+
+local tree_cb = nvim_tree_config.nvim_tree_callback
+
+
 -- recommended settings from nvim-tree documentation
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
