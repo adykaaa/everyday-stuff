54a67d88632b8585ae6ab2bc907553139b3f2d99
adykaaa <gosupalanta@gmail.com>
2022-10-29 07:29:32 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,6 +11,11 @@ mkdir -p ~/.local/share/fonts
 unzip FiraCode.zip -d ~/.local/share/fonts
 fc-cache -fv
 
+#installing lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit && rm lazygit.tar.gz
+
 #installing docker
 sudo apt-get install \
     ca-certificates \
