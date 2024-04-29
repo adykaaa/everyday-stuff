a9d3715f96a9d2ba2346d9815e20fb4ad5fde9f0
adykaaa <adam@alcion.ai>
2024-04-29 07:00:59 +0200

    new colorscheme added

--- nvim/lua/josean/plugins/colorscheme.lua
+++ nvim/lua/josean/plugins/colorscheme.lua
@@ -1,9 +1,23 @@
 return {
+    "Shatur/neovim-ayu",
+    priority = 1000,
+    config = function()
+        require("ayu").setup({
+            mirage = false,
+            terminal = false, -- Set to `false` to let terminal manage its own colors.
+            overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+        })
+        vim.cmd("colorscheme ayu")
+    end,
+}
+
+--[[ Default was this:
+return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
         local transparent = true -- set to true if you would like to enable transparency
-
+ 
         require("tokyonight").setup({
             style = "storm",
             transparent = transparent,
@@ -16,7 +30,7 @@ return {
                 variables = {},
             },
         })
-
+ 
         vim.cmd("colorscheme tokyonight")
     end,
-}
+} ]]
